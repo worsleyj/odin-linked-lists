9b68e6eee5da4dfcd9f01e6838101cc51d78fecd
worsleyj <josephworsley23@gmail.com>
2024-08-27 15:30:12 -0400

    remove unneeded import and comments

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
 import "./reset.css";
 import "./styles.css";
-import { Node } from "./node";
 import { LinkedList } from "./linkedList";
-
-const nodeOne = new Node();
-console.log(nodeOne);
-nodeOne.nextNode = nodeOne;
-console.log(nodeOne);
-
-// const listOne = new LinkedList();
-// listOne.append("e");
-// listOne.append("f");
-// listOne.append("g");
-// console.log(listOne);
-// console.log(listOne.size());
-// // console.log(listOne.contains("f"));
-// // console.log(listOne.find("f"));
-// console.log(listOne.toString());
 
 const list = new LinkedList();
 
