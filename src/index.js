c4e9e8477c85ffce8724a9f1b18041c611df90f8
worsleyj <josephworsley23@gmail.com>
2024-08-27 14:29:09 -0400

    add linkedList class

--- src/index.js
+++ src/index.js
@@ -6,3 +6,21 @@ const nodeOne = new Node();
 console.log(nodeOne);
 nodeOne.nextNode = nodeOne;
 console.log(nodeOne);
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  append(value) {
+    const newNode = new Node();
+    newNode.value = value;
+    if (this.head === null) {
+      this.head = newNode;
+    }
+    console.log(this.head);
+  }
+}
+
+const listOne = new LinkedList();
+listOne.append("e");
