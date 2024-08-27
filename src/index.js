1199ef32fa1886f5f0ba04b45b494dbd52ac93f0
worsleyj <josephworsley23@gmail.com>
2024-08-27 15:29:14 -0400

    move LinkedList class to separate file; test functions

--- src/index.js
+++ src/index.js
@@ -1,109 +1,29 @@
 import "./reset.css";
 import "./styles.css";
 import { Node } from "./node";
+import { LinkedList } from "./linkedList";
 
 const nodeOne = new Node();
 console.log(nodeOne);
 nodeOne.nextNode = nodeOne;
 console.log(nodeOne);
 
-class LinkedList {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-  }
-  append(value) {
-    const newNode = new Node();
-    newNode.value = value;
-    if (this.head === null) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      this.tail.nextNode = newNode;
-      this.tail = newNode;
-    }
-  }
-  prepend(value) {
-    const newNode = new Node();
-    newNode.value = value;
-    if (this.head === null) {
-      this.head = newNode;
-      this.tail = newNode;
-    } else {
-      newNode.nextNode = this.head;
-      this.head = newNode;
-    }
-  }
-  at(index) {
-    if (index > this.size()) return null;
-    let currNode = this.head;
-    while (index > 0) {
-      currNode = currNode.nextNode;
-      index--;
-    }
-    return currNode;
-  }
-  pop() {
-    const newTail = this.at(this.size() - 2);
-    const oldTail = newTail.nextNode;
-    this.tail = newTail;
-    newTail.nextNode = null;
-    return oldTail;
-  }
-  size() {
-    if (this.head === null) return 0;
-    let currNode = this.head;
-    let size = 1;
-    while (currNode.nextNode != null) {
-      size++;
-      currNode = currNode.nextNode;
-    }
-    return size;
-  }
-  contains(value) {
-    let currNode = this.head;
-    let index = this.size();
-    while (index > 0) {
-      if (currNode.value === value) {
-        return true;
-      }
-      currNode = currNode.nextNode;
-      index--;
-    }
-    return false;
-  }
-  find(value) {
-    let currNode = this.head;
-    let index = 0;
-    while (index < this.size()) {
-      if (currNode.value === value) {
-        return index;
-      }
-      currNode = currNode.nextNode;
-      index++;
-    }
-    return null;
-  }
-  toString() {
-    let string = "";
-    let currNode = this.head;
-    let index = 0;
-    while (index < this.size()) {
-      string += "( " + currNode.value + " ) -> ";
-      currNode = currNode.nextNode;
-      index++;
-    }
-    string += null;
-    return string;
-  }
-}
+// const listOne = new LinkedList();
+// listOne.append("e");
+// listOne.append("f");
+// listOne.append("g");
+// console.log(listOne);
+// console.log(listOne.size());
+// // console.log(listOne.contains("f"));
+// // console.log(listOne.find("f"));
+// console.log(listOne.toString());
 
-const listOne = new LinkedList();
-listOne.append("e");
-listOne.append("f");
-listOne.append("g");
-console.log(listOne);
-console.log(listOne.size());
-// console.log(listOne.contains("f"));
-// console.log(listOne.find("f"));
-console.log(listOne.toString());
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+console.log(list.toString());
--- src/linkedList.js
+++ src/linkedList.js
@@ -1,4 +1,94 @@
 import { Node } from "./node";
 
-const nodeOne = new Node();
-console.log(nodeOne);
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+  append(value) {
+    const newNode = new Node();
+    newNode.value = value;
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.nextNode = newNode;
+      this.tail = newNode;
+    }
+  }
+  prepend(value) {
+    const newNode = new Node();
+    newNode.value = value;
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.nextNode = this.head;
+      this.head = newNode;
+    }
+  }
+  at(index) {
+    if (index > this.size()) return null;
+    let currNode = this.head;
+    while (index > 0) {
+      currNode = currNode.nextNode;
+      index--;
+    }
+    return currNode;
+  }
+  pop() {
+    const newTail = this.at(this.size() - 2);
+    const oldTail = newTail.nextNode;
+    this.tail = newTail;
+    newTail.nextNode = null;
+    return oldTail;
+  }
+  size() {
+    if (this.head === null) return 0;
+    let currNode = this.head;
+    let size = 1;
+    while (currNode.nextNode != null) {
+      size++;
+      currNode = currNode.nextNode;
+    }
+    return size;
+  }
+  contains(value) {
+    let currNode = this.head;
+    let index = this.size();
+    while (index > 0) {
+      if (currNode.value === value) {
+        return true;
+      }
+      currNode = currNode.nextNode;
+      index--;
+    }
+    return false;
+  }
+  find(value) {
+    let currNode = this.head;
+    let index = 0;
+    while (index < this.size()) {
+      if (currNode.value === value) {
+        return index;
+      }
+      currNode = currNode.nextNode;
+      index++;
+    }
+    return null;
+  }
+  toString() {
+    let string = "";
+    let currNode = this.head;
+    let index = 0;
+    while (index < this.size()) {
+      string += "( " + currNode.value + " ) -> ";
+      currNode = currNode.nextNode;
+      index++;
+    }
+    string += null;
+    return string;
+  }
+}
+
+export { LinkedList };
